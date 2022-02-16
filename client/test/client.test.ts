05b0b1074d1fc16cf1954ff4a11d5577d689dd96
Martin Westerkamp <westerkamp@tu-berlin.de>
2022-02-16 15:02:58 +0100

    add rejection test

--- client/test/client.test.ts
+++ client/test/client.test.ts
@@ -1,5 +1,5 @@
 import { describe, before } from 'mocha';
-import { expect } from 'chai';
+import { expect, assert } from 'chai';
 import Web3 from 'web3';
 
 import { logger } from '../src/utils/logger';
@@ -18,7 +18,7 @@ describe('Deploy and update relay', () => {
 
         web3 = new Web3(targetUrl);
         [account] = await web3.eth.getAccounts();
-        expect(account).to.be.not.undefined;
+        expect(account).to.exist;
     });
 
     it('should deploy the relay contract', async () => {
@@ -32,7 +32,7 @@ describe('Deploy and update relay', () => {
             sourceUrl,
             targetUrl,
         );
-        expect(relayContractAddress).to.be.not.undefined;
+        expect(relayContractAddress).to.exist;
     });
 
     const updateRelayContract = (i: number) => {
@@ -46,14 +46,31 @@ describe('Deploy and update relay', () => {
                 sourceUrl,
                 targetUrl,
             );
-            expect(txHash).to.be.not.undefined;
+            expect(txHash).to.exist;
             logger.info(`${i}. update txHash: ${txHash}`);
         });
     };
 
     describe('multiple updates', () => {
-        for (let i = 1; i <= 5; i++) {
+        for (let i = 1; i <= 3; i++) {
             updateRelayContract(i);
         }
+
+        it('should reject update skipping subsequennt period', async () => {
+            logger.setSettings({ minLevel: 'info', name: 'reject update' });
+
+            return updateContract(
+                relayContractAddress,
+                syncCommitteePeriod + 1,
+                account,
+                sourceUrl,
+                targetUrl,
+            ).then((txHash) => {
+                assert.fail(`transaction should fail, but was okay at tx: ${txHash}`);
+            }).catch((err: Error) => {
+                expect(err.message).to.have.string('merkle proof for next sync committee not valid');
+                logger.info(`transaction failed as expected: ${err.message}`);
+            });
+        });
     });
 });
